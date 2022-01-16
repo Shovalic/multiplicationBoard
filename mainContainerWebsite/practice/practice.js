9ae8946c87c49cca84e74305dbed3011ea08c337
Shoval Ichia <shovi743@gmail.com>
2022-01-16 16:23:40 +0200

    change upper-caseletters

--- mainContainerWebsite/practice/practice.js
+++ mainContainerWebsite/practice/practice.js
@@ -30,29 +30,8 @@ function done() {
   div.appendChild(e);
   document.getElementById("playagain").style.display = "block";
   return;
+  }
 }
-}
-
-// function done() {
-//     let userAnswer = document.getElementById("resultRandomExercise").value;
-//     let firstNumber = document.getElementById("firstFactor").value;
-//     let secondNumber = document.getElementById("secondFactor").value;
-//     userAnswer = parseInt(userAnswer);
-//     let res = firstNumber * secondNumber;  
-//     if (res == userAnswer){
-//     var e = document.createElement("p");
-//     e.innerHTML = "נָכוֹן מְאוֹד! כָּל הַכָּבוֹד :)";
-//     e.style.marginTop = "10px";
-//     e.style.textAlign = "center";
-//     e.style.direction = "rtl";
-//     e.style.fontSize = "25px";
-//     e.style.fontWeight = "bold";
-//     var div = document.getElementById("randomExercise");
-//     div.appendChild(e);
-//     document.getElementById("playagain").style.display = "block";
-//     return;
-//   }
-// }
 
 function practiceMore() {
     document.getElementById("resultRandomExercise").value = "none";
